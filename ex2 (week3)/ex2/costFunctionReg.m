c88e6e9d506c6f943ff842bdb7a97edcd668c41c
Ane <ane.berasategi@gmail.com>
2017-03-25 16:43:00 +0100

    week3

--- ex2 (week3)/ex2/costFunctionReg.m
+++ ex2 (week3)/ex2/costFunctionReg.m
@@ -17,9 +17,13 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+h = sigmoid(X*theta);
 
+J = (-1/m)*(y'*log(h) + (1-y)'*log(1-h)) + (lambda/2/m)*(sum(theta.^2) - theta(1)^2);
 
+grad = (1/m)*X'*(h-y) + (lambda/m)*theta;
 
+grad(1) = (1/m)*X(1)*sum(h-y);
 
 
 % =============================================================
